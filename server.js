0f20b13b6384cc3cd46a4479c181d8e012618510
Vishal <vishalpraneeth7771@gmail.com>
2023-07-27 15:29:48 +0530

    server.js

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const dotenv = require("dotenv").config();
 const port = process.env.PORT || 5000;
 const app = express();
 
+app.use("/api/contacts", require("./routes/contactroute"));
+
 app.listen(port, () => {
     console.log('Server running on port: ${port}');
 });
