444ce9e6e18950e568f4f70f6c84446026c88bb9
Vishal <vishalpraneeth7771@gmail.com>
2023-07-29 17:22:06 +0530

    connect db function

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require("express");
+const errorHandler = require("./middleware/errorhandler");
+const connectdb = require("./config/dbconnection");
 const dotenv = require("dotenv").config();
 const port = process.env.PORT || 5000;
 const app = express();
+connectdb();
 app.use(express.json());
 app.use("/api/contacts", require("./routes/contactroute"));
+app.use(errorHandler);
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`); 
